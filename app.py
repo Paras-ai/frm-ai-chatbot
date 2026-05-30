38a88da17780fbce3026c348a8528c6b8c05473c
Paras-ai <paraskhurana1995@gmail.com>
2026-05-30 21:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-pro")
 
 # ---------- EMBEDDINGS MODEL ----------
 embedding = HuggingFaceEmbeddings(
